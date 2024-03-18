636d87a4269553b7b26af404aa13290608bdc91c
Varun Dhand <varundhand77@gmail.com>
2024-03-18 14:27:30 +0530

    Feat: POST request to apiGateway

---
+++ script.js
@@ -0,0 +1,21 @@
+// callAPI function that takes the base and exponent numbers as parameters
+var callAPI = (base,exponent)=>{
+    // instantiate a headers object
+    var myHeaders = new Headers();
+    // add content type header to object
+    myHeaders.append("Content-Type", "application/json");
+    // using built in JSON utility package turn object to string and store in a variable
+    var raw = JSON.stringify({"base":base,"exponent":exponent});
+    // create a JSON object with parameters for API call and store in a variable
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+    // make API call with parameters and use promises to get response
+    fetch("Yhttps://3b7y6pb5pl.execute-api.ap-southeast-2.amazonaws.com/dev", requestOptions)
+    .then(response => response.text())
+    .then(result => alert(JSON.parse(result).body))
+    .catch(error => console.log('error', error));
+}
